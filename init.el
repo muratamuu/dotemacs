0b4cf6818245c32d59c6b473797def1a89062008
muratamuu <murata.muu@gmail.com>
2015-12-06 00:24:10 +0900

    ハイライトなど追加

--- init.el
+++ init.el
@@ -49,9 +49,15 @@
 ;;  (format "%%f - Emacs@%s" (system-name)))
 
 ;; バックアップファイルを作らない
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-(setq delete-auto-save-files t)
+;; (setq make-backup-files nil)
+;; (setq auto-save-default nil)
+;; (setq delete-auto-save-files t)
+
+;; バックアップとオートセーブファイルを~/.emacs.d/backupsへ集める
+(add-to-list 'backup-directory-alist
+             (cons "." "~/.emacs.d/backups/"))
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
 
 ;; タブをスペースに変換する
 (setq-default tab-width 4 indent-tabs-mode nil)
@@ -65,3 +71,31 @@
 
 ;; 折り返しトグルコマンド
 (define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+
+;; 現在行のハイライト
+(defface my-hl-line-face
+  ;; 背景がdarkならば背景色を紺に
+  '((((class color) (background dark))
+     (:background "NavyBlue" t))
+    ;; 背景色がlightならば背景色を緑に
+    (((class color) (background light))
+     (:background "LightGoldenrodYellow" t))
+    (t (:bold t)))
+  "hl-line's my-face")
+(setq hl-line-face 'my-hl-line-face)
+(global-hl-line-mode t)
+
+;; paren-mode:対応する括弧を強調して表示する
+(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
+(show-paren-mode t) ; 有効化
+;; parenのスタイル: expressionは括弧内も強調表示
+(setq show-paren-style 'expression)
+;; フェイスを変更する
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "yellow")
+
+;; カラム番号もモードラインに表示する
+(column-number-mode t)
+
+;; ファイルサイズをモードラインに表示する
+(size-indication-mode t)
