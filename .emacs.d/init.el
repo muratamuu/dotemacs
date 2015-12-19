b56f69b67262b67928800dff7108ecc6ffff8224
muratamuu <murata.muu@gmail.com>
2015-12-19 21:46:47 +0900

    change color

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -301,17 +301,17 @@
 ;;(set-face-background 'region "darkgreen")
 
 ;; 現在行のハイライト
-(defface my-hl-line-face
-  ;; 背景がdarkならば背景色を紺に
-  '((((class color) (background dark))
-     (:background "DarkSlateBlue" t))
-    ;; 背景色がlightならば背景色を緑に
-    (((class color) (background light))
-     (:background "#999999" t))
-    (t (:bold t)))
-  "hl-line's my-face")
-(setq hl-line-face 'my-hl-line-face)
-(global-hl-line-mode t)
+;; (defface my-hl-line-face
+;;   ;; 背景がdarkならば背景色を紺に
+;;   '((((class color) (background dark))
+;;      (:background "DarkSlateBlue" t))
+;;     ;; 背景色がlightならば背景色を緑に
+;;     (((class color) (background light))
+;;      (:background "#FFFFFF" t))
+;;     (t (:bold t)))
+;;   "hl-line's my-face")
+;; (setq hl-line-face 'my-hl-line-face)
+;; (global-hl-line-mode t)
 
 ;; paren-mode:対応する括弧を強調して表示する
 (setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
@@ -322,8 +322,8 @@
 ;;(set-face-background 'show-paren-match-face nil)
 ;;(set-face-underline-p 'show-paren-match-face "yellow")
 (set-face-attribute 'show-paren-match-face nil
-                    :background "#888800" :foreground nil
-                    :underline "#888800" :weight 'extra-bold)
+                    :background nil :foreground nil
+                    :underline nil :weight 'extra-bold)
 
 ;; カラム番号もモードラインに表示する
 (column-number-mode t)
